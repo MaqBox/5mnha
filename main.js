7ff273a13d99a71eb05d5e2466bb505491590fa8
MaqBox <magrenbox@gmail.com>
2025-08-09 13:17:37 +0300

    Fix higher-lower mode image clickability and timing issues

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ import {
     submitButton,
     backToCategoriesBtn,
     initializeUIElements  // Add this import
-} from './js/components/uimanager.js';
+} from './js/components/uiManager.js';
 import { setupInputValidation, validateGuess } from './js/components/inputHandler.js';
 import {
     initHigherLowerMode,
